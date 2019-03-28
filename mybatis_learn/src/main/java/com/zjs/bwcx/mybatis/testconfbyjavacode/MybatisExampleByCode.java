63d713eeca59f54ec91d5b74c860547e89c01315
shizhijie <2272684702@qq.com>
2019-03-28 15:58:28 +0800

    xml && java code config mybatis

---
+++ mybatis_learn/src/main/java/com/zjs/bwcx/mybatis/testconfbyjavacode/MybatisExampleByCode.java
@@ -0,0 +1,44 @@
+package com.zjs.bwcx.mybatis.testconfbyjavacode;
+
+import org.apache.ibatis.datasource.pooled.PooledDataSource;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.TransactionFactory;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+
+import com.zjs.bwcx.mybatis.mapper.RoleMapper;
+import com.zjs.bwcx.mybatis.pojo.Role;
+
+public class MybatisExampleByCode {
+	
+	public static void main(String[] args) {
+		SqlSessionFactory sqlSessionFactory = init();
+		RoleMapper mapper = sqlSessionFactory.openSession().getMapper(RoleMapper.class);
+		Role role = mapper.getRole(1);
+		System.out.println(role);
+	}
+	
+	public static SqlSessionFactory init() {
+		//构建数据库连接池
+		PooledDataSource dataSource = new PooledDataSource();
+		dataSource.setDriver("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/guns?serverTimezone=UTC");
+		dataSource.setUsername("root");
+		dataSource.setPassword("123");
+		//构建数据库事物方式
+		TransactionFactory transactionFactory = new JdbcTransactionFactory();
+		//创建数据库运行环境
+		Environment environment = new Environment("development", transactionFactory, dataSource);
+		//构建Configuration对象
+		Configuration configuration = new Configuration(environment);
+		//注册一个mybatis上下文别名
+		configuration.getTypeAliasRegistry().registerAlias("role",Role.class);
+		//加入一个映射器
+		configuration.addMapper(RoleMapper.class);
+		//使用SqlSessionFactoryBuilder构建SqlSessionFactory
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+		return sqlSessionFactory;
+	}
+}
---
+++ mybatis_learn/src/main/java/com/zjs/bwcx/mybatis/testconfbyxml/MybatisExample.java
@@ -0,0 +1,21 @@
+package com.zjs.bwcx.mybatis.testconfbyxml;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.zjs.bwcx.mybatis.mapper.RoleMapper;
+import com.zjs.bwcx.mybatis.pojo.Role;
+import com.zjs.bwcx.mybatis.utils.MyBatisUtil;
+
+public class MybatisExample {
+	public static void main(String[] args) {
+		SqlSession sqlSession = null;
+		try {
+			sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+			RoleMapper mapper = sqlSession.getMapper(RoleMapper.class);
+			Role role = mapper.getRole(1);
+			System.out.println(role);
+		} finally {
+			sqlSession.close();
+		}
+	}
+}
