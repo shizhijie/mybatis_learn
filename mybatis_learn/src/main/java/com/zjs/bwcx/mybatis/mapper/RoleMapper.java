664a434ae8da33da860f3ee7b631ceee432ca48d
shizhijie <2272684702@qq.com>
2019-03-28 16:01:31 +0800

    update RoleMapper.java

--- mybatis_learn/src/main/java/com/zjs/bwcx/mybatis/mapper/RoleMapper.java
+++ mybatis_learn/src/main/java/com/zjs/bwcx/mybatis/mapper/RoleMapper.java
@@ -1,7 +1,11 @@
 package com.zjs.bwcx.mybatis.mapper;
 
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
 import com.zjs.bwcx.mybatis.pojo.Role;
 
 public interface RoleMapper {
-	public Role getRole(/*@Param("id")*/ int id);
+	@Select("select * from sys_role where id=#{id}")
+	public Role getRole(@Param("id") int id);
 }
